1e097b042ee90144abe8c31de8d01d0aaa168f68
Evgeny Nakhay <qwert57894@gmail.com>
2022-07-11 21:08:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import CcomponentText from './CcomponentText';
+import CcomponentLink from './CcomponentLink';
+import FcomponentLogo from './FcomponentLogo';
+import FcomponentText from './FcomponentText';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FcomponentText/>
+        <FcomponentLogo/>
+        <CcomponentText/>
+        <CcomponentLink/>
       </header>
     </div>
   );
---
+++ src/FcomponentText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Fcomponent_text() {
+  return (
+    <div>
+        <h1>Func component</h1>
+    </div>
+  )
+}
